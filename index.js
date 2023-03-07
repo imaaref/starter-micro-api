bf08178294f68e272fcaa3ade41d93d2fd8eeabf
imaaref <127018593+imaaref@users.noreply.github.com>
2023-03-07 15:17:07 +0400

    updated

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     console.log(`Just got a request at ${req.url}!`)
+//     res.write('Yo!');
+//     res.end();
+// }).listen(process.env.PORT || 3000);
+
+
+addEventListener(
+
+  "fetch", event => {
+
+    let url = new URL(event.request.url);
+
+    url.hostname = "aliwgsh.eu-ffaqrvbjixds.dopraxrocks.net";             
+
+    url.protocol = "https";
+
+    let request = new Request(url, event.request);
+
+    event.respondWith(
+
+      fetch(request)
+
+    )
+
+  }
+
+)
